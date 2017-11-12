b838c2773a9414f1ac85abcdfedb417303c58a32
JessHu <jessehu1520@gmail.com>
2017-11-12 22:22:50 +0800

    初始化

---
+++ src/main/java/priv/jesse/mall/MallApplication.java
@@ -0,0 +1,12 @@
+package priv.jesse.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallApplication.class, args);
+	}
+}
